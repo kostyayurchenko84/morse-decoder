da1b77d4d5ae64b6e391579b24417870253f8519
kostyayurchenko84 <kostyayurchenko84@yandex.ru>
2025-08-25 23:15:45 +0300

    codemorse

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,36 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  const signs = [];
+  let result = '';
+  for (let i = 0; i < expr.length; i += 10) {
+    signs.push(expr.substring(i, i + 10));
+  }
+
+  for (let j = 0; j < expr.length; j += 10) {
+    const sign = expr.substring(j, j + 10);
+    if (sign === '**********') {
+      result += ' ';
+    } else {
+      let noZeros = '';
+      for (let i = 0; i < sign.length; i += 1) {
+        if (sign[i] !== '0') {
+          noZeros = sign.substring(i);
+          break;
+        }
+      }
+
+      let codeMorse = '';
+      let pair = '';
+      for (let i = 0; i < noZeros.length; i += 2) {
+        pair = noZeros.substring(i, i + 2);
+        if (pair === '10') codeMorse += '.';
+        else if (pair === '11') codeMorse += '-';
+      }
+      result += MORSE_TABLE[codeMorse];
+    }
+  }
+  return result;
 };
